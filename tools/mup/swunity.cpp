08a7f2ddf877cda86e48069071e2c416485f0823
Persian Prince <persianpros@yahoo.com>
2019-12-18 02:21:07 +0330

    Update swunity.cpp

--- tools/mup/swunity.cpp
+++ tools/mup/swunity.cpp
@@ -300,7 +300,7 @@ void SwUnity::extract()
 			vHelpStr[i] = '_';
 		}
 	}
-	fd = open(vHelpStr, O_WRONLY | O_CREAT,S_IRUSR|S_IWUSR));
+	fd = open(vHelpStr, O_WRONLY | O_CREAT,S_IRUSR|S_IWUSR);
 	if (fd < 0)
 	{
 		printf("error opening %s\n", vHelpStr);
